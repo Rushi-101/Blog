7ca412c7035e5115a869881adbb76554e9ae5a3d
Rushikesh J. Metkar <rushijm002@gmail.com>
2020-08-05 18:52:15 +0530

    added django-heroku

--- django_web/settings.py
+++ django_web/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +26,7 @@ SECRET_KEY = 'kzy*m*r@7ssp%-z9z(mm11d!wnsg8_243tuwt1xj)u)wh4wsv2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['rushikeshblogapp.herokuapp.com']
 
 
 # Application definition
@@ -139,3 +139,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
+django_heroku.settings(locals())
